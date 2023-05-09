e2b53ae7078716fe351dc8ff96ac032ef34149cc
Konrad Korus <konradkor@interia.pl>
2023-05-09 17:48:57 +0200

    add basic routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { NavLink, Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <NavLink to="/"> CinemaName</NavLink>
+      <NavLink to="/repertoire"> Repertuar</NavLink>
+      <Link to="#footer"> Kontakt</Link>
+      <NavLink to="/login"> Zaloguj się</NavLink>
+      <NavLink to="/register"> Zarejestruj się</NavLink>
+    </>
   )
 }
 
