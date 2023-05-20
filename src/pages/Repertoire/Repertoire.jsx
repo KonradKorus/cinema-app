59ab61f04d7da56818e164743ddf6f9d64d55377
xShadeen <126704503+xShadeen@users.noreply.github.com>
2023-05-20 16:59:38 +0200

    reservation
    Reservation page created

--- src/pages/Repertoire/Repertoire.jsx
+++ src/pages/Repertoire/Repertoire.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Repertoire = () => {
-  return (
-    <div>Repertoire</div>
-  )
-}
 
-export default Repertoire
+  return (
+    <div>
+      <h1>Reservation</h1>
+      <Link to="/reservation">
+        <button>Przejdź do strony rezerwacji</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Repertoire;
--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './seats.css'
+import Button from '../Reservation/Button'
+
+const reserved = [
+  { row: 4, seat: 11 },
+  { row: 3, seat: 1 },
+  { row: 5, seat: 6 },
+  { row: 1, seat: 5 },
+  { row: 3, seat: 7 },
+  { row: 3, seat: 8 }
+]
 
 const Reservation = () => {
+  const [showSeats, setShowSeats] = useState(false)
+
+  const handleSeatsClick = () => {setShowSeats(true)}
+
+  const selectedSeats = []
+  const handleSeatClick = (id, isSelected) => {isSelected ? selectedSeats.push(id) : selectedSeats.pop(id) }
+
+  const findReservedSeatsID = (seats) => {
+    const reservedSeats = seats.map((element) => (element.row - 1) * 12 + element.seat)
+    return reservedSeats
+  }
+
+  const renderRow = (rowNumber) => {
+    const seats = []
+    for (let i = 0; i < 12; i++) {
+      const id = rowNumber * 12 + i+1
+      seats.push(
+        <Button
+          key={id}
+          id={id}
+          className='seatBlack'
+          text={i}
+          handleSeatClick={handleSeatClick}
+        />
+      )
+    }
+
+    const reservedSeats = findReservedSeatsID(reserved)
+
+    const updatedSeats = seats.map((button) => {
+      if (reservedSeats.includes(button.props.id)) {
+        return React.cloneElement(button, { className: 'seatRed' })
+      } else {
+        return button
+      }
+    })
+
+    return updatedSeats
+  }
+
+  const renderSeats = () => {
+    const seats = []
+    for (let i = 0; i < 6; i++) {
+      seats.push(<div className="row">{renderRow(i)}</div>)
+    }
+    return seats
+  }
+
   return (
-    <div>Reservation</div>
+    <div>
+      {!showSeats && (
+        <button onClick={handleSeatsClick}>Pokaż salę kinową</button>
+      )}
+      {showSeats && (
+        <div>
+          <div className='main'>
+            <p>Tytul</p>
+            <p>Napisy/2D</p>
+            <p>Data, godzina</p>
+            <p>Sala 1 ile wolnych miejsc</p>
+            <div className="screen">
+              <span>Ekran</span>
+            </div>
+          </div>
+          <div>
+            {renderSeats()}
+          </div>
+          <div className='main'>
+            <button>Zarezerwuj</button>
+          </div>
+        </div>
+      )}
+    </div>
+
   )
 }
 
