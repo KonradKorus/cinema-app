467efd9604684966c4361abb8c6fa78f91dd8ceb
Konrad Korus <konradkor@interia.pl>
2023-05-30 11:01:49 +0200

    fix small console errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -107,9 +107,9 @@ const Footer = () => {
             width="350"
             height="250"
             style={{ border: 0 }}
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </Box>
       </Box>
