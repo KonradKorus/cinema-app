7c9ea7e5eb2d1b97c9eb3e0c534930ee250ff498
MATT6007 <MATT6007@users.noreply.github.com>
2023-06-07 11:55:45 +0200

    Add css to display film info

--- src/pages/MovieDescription/MovieDescription.jsx
+++ src/pages/MovieDescription/MovieDescription.jsx
@@ -4,22 +4,8 @@ import { moviesMock } from '../../utils/moviesMock';
 import YouTube from 'react-youtube';
 import './MovieDescription.css';
 
-
-/*const MovieDescription = () => {
-  const { movieId } = useParams();
-
-  return <div>MovieDescription/{movieId}</div>;
-};
-
-export default MovieDescription;
-
-*/
-
-
 const MovieDescription = () => {
-  //console.log(moviesMock.find((film) => film.id === 1))
-  const param = useParams()
-  const movieId = param.movieId
+  const { movieId } = useParams();
   const film = moviesMock.find((film) => film.id === parseInt(movieId, 10));
 
   if (!film) {
@@ -31,23 +17,27 @@ const MovieDescription = () => {
   }
 
   return (
-    <div>
-      <div className="film-container">
-        <div className="film-image">
-          <img src={film.photo} alt={film.title} />
-        </div>
-        <div className="film-info">
-          <h2>{film.title}</h2>
-          <p>Kategoria: {film.genre}</p>
-          <p>Wiek: {film.ageRestriction}</p>
-          <p>Czas trwania: {film.duration}</p>
-          <p>Napisy: {film.subtitles}</p>
+    <div className='main-container'>
+      <div className='upper-container'>
+        <img src={film.photo} alt={film.title} />
+        <div className='whole-info-container'>
+          <div className="film-info">
+            <h2>{film.title}</h2>
+            <p>Kategoria: {film.genre}</p>
+            <p>Wiek: {film.ageRestriction}</p>
+            <p>Czas trwania: {film.duration}</p>
+            <p>Napisy: {film.subtitles}</p>
+          </div>
+          <div className="film-description">
+            <p>{film.description}</p>
+          </div>
         </div>
       </div>
+
       <div className="film-player">
         <YouTube videoId={film.trailerLink} />
       </div>
-    </div>
+    </div >
   );
 }
 
