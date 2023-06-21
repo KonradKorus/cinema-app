294aab22b56503765af9e08d1cc3e79b777d8634
MATT6007 <MATT6007@users.noreply.github.com>
2023-06-21 21:09:29 +0200

    Final changes

--- src/pages/MovieDescription/MovieDescription.jsx
+++ src/pages/MovieDescription/MovieDescription.jsx
@@ -37,6 +37,7 @@ const MovieDescription = () => {
       <div className="film-player">
         <YouTube videoId={film.trailerLink} />
       </div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/97zD7xeqSZY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div >
   );
 }
